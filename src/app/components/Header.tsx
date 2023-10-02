4bc9cd2eddf0e56ab724aaf2a5fb821df3c97c33
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-02 20:05:31 -0300

    update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import logo from '@/assets/logo.svg'
 import { IoMailOutline } from 'react-icons/io5'
+import { BiLogoWhatsapp } from 'react-icons/bi'
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center justify-center px-4 py-2">
+    <header className="flex flex-col gap-2 font-normal text-slate-50 items-center justify-center px-4 py-2 bg-slate-900 font-roboto">
       <Image src={logo} alt="Logo" />
       {/* Div Links */}
       <div className="flex w-full items-center justify-center gap-2">
@@ -12,6 +13,11 @@ export function Header() {
         <div className="flex gap-1 items-center">
           <IoMailOutline className="text-orange-300" />
           <span>advbrunosilva@outlook.com</span>
+        </div>
+
+        <div className="flex gap-1 items-center">
+          <BiLogoWhatsapp className="text-orange-300" />
+          <span>86 99567 - 6008</span>
         </div>
       </div>
     </header>
