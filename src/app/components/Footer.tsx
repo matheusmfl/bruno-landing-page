02d276027ded4a5b39967e1879c086f7f03cbc2f
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-06 18:10:22 -0300

    update Footer, create footer desktop screen

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,8 +3,8 @@ import { PiMapPinDuotone } from 'react-icons/pi'
 
 export function Footer() {
   return (
-    <footer className="flex flex-col gap-5 py-8 px-4 border-t-2 border-white bg-slate-900">
-      <div className="flex flex-col gap-3">
+    <footer className="flex flex-col gap-5 py-8 px-4 border-t-2 border-white bg-slate-900 lg:px-32 lg:py-14">
+      <div className="flex flex-col gap-3 lg:flex-row lg:w-full lg:justify-between">
         <div className="flex gap-2">
           <AiOutlineMail className="w-[24px] h-[24px] text-[#FDBA74]" />
           <span className="text-slate-50 font-roboto text-base font-normal">
@@ -21,11 +21,15 @@ export function Footer() {
         <div className="flex gap-2">
           <PiMapPinDuotone className="w-[24px] h-[24px] text-[#FDBA74]" />
           <span className="text-slate-50 font-roboto text-base font-normal">
-            Rua Dez - (Parque Karina), <br /> Boa Esperança, <br />
+            Rua Dez - (Parque Karina), <br className="lg:hidden " /> Boa
+            Esperança, <br />
             Timon - MA - CEP 65636-835
           </span>
         </div>
       </div>
+      <span className="text-orange-300 text-base font-roboto font-normal leading-[24px] text-center">
+        © 2023 Bruno Silva - Advocacia. Todos os direitos reservados
+      </span>
     </footer>
   )
 }
