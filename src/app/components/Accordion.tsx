485c139fa8162a305f0e8d23c1a256205d1867a0
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-07 18:42:45 -0300

    update accordion Trigger

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -22,15 +22,18 @@ export function AccordionComponent() {
                 className="group"
                 key={item.id}
               >
-                <Accordion.Header className="bg-slate-100  transition-transform group-data-[state=open]:bg-slate-800 py-[22px] px-[20px] shadow-xl  flex gap-[18px] items-center lg:items-start lg:justify-between justify-center">
-                  <span className="text-slate-700 group-data-[state=open]:text-orange-300 transition-colors duration-300 font-roboto text-xl font-medium leading-[24px]">
-                    {item.title}
-                  </span>
+                <Accordion.Trigger className="w-full">
+                  <Accordion.Header className="bg-slate-100  transition-transform group-data-[state=open]:bg-slate-800 py-[22px] px-[20px] shadow-xl  flex gap-[18px] items-center lg:items-start lg:justify-between justify-center">
+                    <span className="text-slate-700 group-data-[state=open]:text-orange-300 transition-colors duration-300 font-roboto text-xl font-medium leading-[24px]">
+                      {item.title}
+                    </span>
 
-                  <Accordion.Trigger className="h-full group text-orange-300">
-                    <BiSolidChevronDown className="text-slate-700 w-[24px] ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:text-orange-300 rotate-0 group-data-[state=open]:rotate-180 h-[24px]" />
-                  </Accordion.Trigger>
-                </Accordion.Header>
+                    <Accordion.Trigger className="h-full group">
+                      <BiSolidChevronDown className="text-slate-700 w-[24px] ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:text-orange-300 rotate-0 group-data-[state=open]:rotate-180 h-[24px]" />
+                    </Accordion.Trigger>
+                  </Accordion.Header>
+                </Accordion.Trigger>
+
                 <Accordion.Content className="bg-slate-100 shadow-md overflow-hidden py-4 px-6 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp">
                   <span className="font-roboto text-slate-700 font-medium text-xl leading-[28px]">
                     {item.content}
