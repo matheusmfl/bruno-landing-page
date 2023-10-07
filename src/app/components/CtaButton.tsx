e92abb88faa342b2a67b21b364508d77e15a131d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-07 18:37:36 -0300

    update cel phone

--- src/app/components/CtaButton.tsx
+++ src/app/components/CtaButton.tsx
@@ -5,7 +5,7 @@ export function CtaButton() {
   return (
     <Link
       href={
-        'https://wa.me/5585985676008?text=Dr.+Bruno+Silva%2C+preciso+de+ajuda+com+meu+ve%C3%ADculo%C2%A0URGENTE%21%21'
+        'https://wa.me/5586995676008?text=Dr.+Bruno+Silva%2C+preciso+de+ajuda+com+meu+ve%C3%ADculo%C2%A0URGENTE%21%21'
       }
     >
       <button className="flex rounded-[4px] gap-[10px] lg:w-[280px] bg-green-700 items-center justify-center w-full py-3 font-roboto text-slate-50 font-medium text-base">
