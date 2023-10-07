2807b1eb71054b4e5c5eaf8da803575720dcef18
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-07 16:57:40 -0300

    input the video

--- src/app/components/VideoSection.tsx
+++ src/app/components/VideoSection.tsx
@@ -3,7 +3,7 @@ import { CtaButton } from './CtaButton'
 
 export function VideoSection() {
   return (
-    <section className="flex flex-col bg-slate-200 py-8 px-4 gap-4 font-roboto lg:flex-row lg:px-32 lg:py-14 lg:pb-0">
+    <section className="flex flex-col bg-slate-200 py-8 px-4 gap-4 font-roboto lg:flex-row lg:px-32 lg:py-14">
       {/* <Image
         src={thumb}
         alt="thumb de video"
@@ -11,11 +11,13 @@ export function VideoSection() {
         height={362}
         className="aspect-square object-contain object-left-top"
       /> */}
-      <iframe
-        width="560"
-        height="315"
-        src="https://youtube.com/shorts/JXVo9Hj0-uk"
-      ></iframe>
+      {/* Div do vídeo */}
+      <div>
+        <iframe
+          className="aspect-video w-full"
+          src="https://player.vimeo.com/video/872097003"
+        ></iframe>
+      </div>
 
       <div className="lg:flex lg:flex-col lg:gap-6 lg:pt-5">
         <h2 className="text-amber-600 text-2xl font-medium leading-[28px] pb-2">
