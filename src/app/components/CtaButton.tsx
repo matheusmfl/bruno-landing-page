270ce128e329b5007aaf658482a945a4b3bfcd9e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-09 20:31:28 -0300

    update Aria-label

--- src/app/components/CtaButton.tsx
+++ src/app/components/CtaButton.tsx
@@ -8,7 +8,10 @@ export function CtaButton() {
         'https://wa.me/5586995676008?text=Dr.+Bruno+Silva%2C+preciso+de+ajuda+com+meu+ve%C3%ADculo%C2%A0URGENTE%21%21'
       }
     >
-      <button className="flex rounded-[4px] gap-[10px] lg:w-[280px] bg-green-700 items-center justify-center w-full py-3 font-roboto text-slate-50 font-medium text-base">
+      <button
+        aria-label="Teste"
+        className="flex rounded-[4px] gap-[10px] lg:w-[280px] bg-green-700 items-center justify-center w-full py-3 font-roboto text-slate-50 font-medium text-base"
+      >
         Quero ajuda especializada{' '}
         <BiLogoWhatsapp className="text-slate-50 text-2xl" />
       </button>
