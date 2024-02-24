c5e2434a9ccd3f4d4a713a8a2d113908df3cd910
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-24 20:36:23 -0300

    feat: add hover on CtaButton

--- src/app/components/CtaButton.tsx
+++ src/app/components/CtaButton.tsx
@@ -10,7 +10,7 @@ export function CtaButton() {
     >
       <button
         aria-label="Teste"
-        className="flex rounded-[4px] gap-[10px] lg:w-[280px] bg-green-700 items-center justify-center w-full py-3 font-roboto text-slate-50 font-medium text-base"
+        className="flex rounded-[4px] gap-[10px] lg:w-[280px] bg-green-700 hover:bg-green-600 items-center justify-center w-full py-3 font-roboto text-slate-50 font-medium text-base"
       >
         Quero ajuda especializada{' '}
         <BiLogoWhatsapp className="text-slate-50 text-2xl" />
