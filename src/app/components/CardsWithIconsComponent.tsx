9d9908bb231a15f8e11befbeeadc8e7dece2fd33
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-25 16:51:23 -0300

    refactor: add text-color

--- src/app/components/CardsWithIconsComponent.tsx
+++ src/app/components/CardsWithIconsComponent.tsx
@@ -27,7 +27,7 @@ export function CardsWithIconsComponent({
   return (
     <div className="border-[4px] relative border-slate-400 py-[45px] rounded-md flex flex-col gap-6 px-6">
       <h2 className="text-[20px] leading-6 font-bold text-center">{title}</h2>
-      <span className="text-[20px] leading-6 font-medium text-center">
+      <span className="text-[20px] leading-6 font-medium text-center text-slate-50">
         {subtitle}
       </span>
 
